aedb9a4dcc2ff2dc93edaa54afc1b31f56165d8b
Melchizedek-1 <31878736+Melchizedek-1@users.noreply.github.com>
2021-06-26 06:00:24 +0300

    reviews backend

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/productController')
+const { getProducts, newProduct, getSingleProduct, deleteReview, getProductReviews, updateProduct, deleteProduct, createProductReview } = require('../controllers/productController')
 
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth')
 
@@ -9,5 +9,8 @@ router.route('/products').get(isAuthenticatedUser, getProducts)
 router.route('/product/:id').get(getSingleProduct)
 router.route('/admin/product/new').post(isAuthenticatedUser, authorizeRoles('admin'), newProduct)
 router.route('/admin/product/:id').put(isAuthenticatedUser, authorizeRoles('admin'), updateProduct).delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProduct)
+router.route('/review').put(isAuthenticatedUser, createProductReview)
+router.route('/reviews').get(isAuthenticatedUser, getProductReviews)
+router.route('/reviews').delete(isAuthenticatedUser, deleteReview)
 
 module.exports = router
