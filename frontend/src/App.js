a7dc871f52792ede581b85c145dca185f14af8ad
Melchizedek-1 <31878736+Melchizedek-1@users.noreply.github.com>
2021-06-28 18:25:15 +0300

    login component and functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/layouts/Header';
 import Footer from './components/layouts/Footer';
 import Home from './components/Home';
 import ProductDetails from './components/product/ProductDetails';
+import Login from './components/user/Login';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" component={Home} exact />
           <Route path="/search/:keyboard" component={Home} />
           <Route path="/product/:id" component={ProductDetails} exact />
+          <Route path="/login" component={Login} />
         </div>
         <Footer />
       </div>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,15 +2,17 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productsReducer, productDetailsReducer } from './reducers/productReducers'
+import { authReducer } from './reducers/userReducer'
 
 const reducer = combineReducers({
     products: productsReducer,
-    productDetails: productDetailsReducer
+    productDetails: productDetailsReducer,
+    auth: authReducer
 })
 
 let initialState = {}
 
-const middleware = [thunk]
+const middleware = [thunk];
 const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
 
-export default store
+export default store;
