3c06d8c26741d51be7860d2516388cb6c5a67ed9
Melchizedek-1 <31878736+Melchizedek-1@users.noreply.github.com>
2021-07-04 08:54:24 +0300

    checkout steps component

--- frontend/src/components/cart/Shipping.js
+++ frontend/src/components/cart/Shipping.js
@@ -1,6 +1,7 @@
 import React, {Fragment, useState} from 'react'
 import { countries } from 'countries-list'
 import MetaData from '../layouts/MetaData'
+import CheckoutSteps from './CheckoutSteps'
 import { useDispatch, useSelector } from 'react-redux'
 import { saveShippingInfo } from '../../actions/cartActions'
 
@@ -27,6 +28,7 @@ const Shipping = ({ history }) => {
     return (
         <Fragment>
             <MetaData title={'Your Shipping Info'} />
+            <CheckoutSteps shipping />
             <div className="row wrapper">
                 <div className="col-10 col-lg-5">
                     <form className="shadow-lg" onSubmit={submitHandler}>
