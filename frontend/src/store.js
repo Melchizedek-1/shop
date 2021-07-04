15a14acda43b79939a6cf76ca49c2b6fc2a417dc
Melchizedek-1 <31878736+Melchizedek-1@users.noreply.github.com>
2021-07-04 15:24:38 +0300

    Logged in users orders

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,7 +4,7 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import { productsReducer, productDetailsReducer } from './reducers/productReducers'
 import { authReducer, userReducer, forgotPasswordReducer } from './reducers/userReducer'
 import { cartReducer } from './reducers/cartReducers'
-import { newOrderReducer } from './reducers/orderReducers'
+import { newOrderReducer, myOrdersReducer } from './reducers/orderReducers'
 
 const reducer = combineReducers({
     products: productsReducer,
@@ -13,7 +13,8 @@ const reducer = combineReducers({
     user: userReducer,
     forgotPassword: forgotPasswordReducer,
     cart: cartReducer,
-    newOrder: newOrderReducer
+    newOrder: newOrderReducer,
+    myOrders: myOrdersReducer
 })
 
 let initialState = {
