4b06f013e544f0365d18902ee03a2b74e9bc23fd
Melchizedek-1 <31878736+Melchizedek-1@users.noreply.github.com>
2021-07-05 20:15:53 +0300

    Display all orders

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -10,6 +10,9 @@ import {
     ORDER_DETAILS_REQUEST,
     ORDER_DETAILS_SUCCESS,
     ORDER_DETAILS_FAIL,
+    ALL_ORDERS_REQUEST,
+    ALL_ORDERS_SUCCESS,
+    ALL_ORDERS_FAIL,
     CLEAR_ERRORS
 } from '../constants/orderConstants'
 
@@ -68,6 +71,24 @@ export const getOrderDetails = (id) => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: ORDER_DETAILS_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
+
+export const allOrders = () => async (dispatch) => {
+    try {
+        dispatch({ type: ALL_ORDERS_REQUEST });
+        const { data } = await axios.get(`/api/v1/admin/orders`)
+
+        dispatch({
+            type: ALL_ORDERS_SUCCESS,
+            payload: data
+        })
+
+    } catch (error) {
+        dispatch({
+            type: ALL_ORDERS_FAIL,
             payload: error.response.data.message
         })
     }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,7 +4,7 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import { productsReducer, productDetailsReducer, productReducer, newProductReducer, newReviewReducer } from './reducers/productReducers'
 import { authReducer, userReducer, forgotPasswordReducer } from './reducers/userReducer'
 import { cartReducer } from './reducers/cartReducers'
-import { newOrderReducer, myOrdersReducer, orderDetailsReducer } from './reducers/orderReducers'
+import { newOrderReducer, myOrdersReducer, orderDetailsReducer, allOrdersReducer } from './reducers/orderReducers'
 
 const reducer = combineReducers({
     products: productsReducer,
@@ -17,6 +17,7 @@ const reducer = combineReducers({
     cart: cartReducer,
     newOrder: newOrderReducer,
     myOrders: myOrdersReducer,
+    allOrders: allOrdersReducer,
     orderDetails: orderDetailsReducer,
     newReview: newReviewReducer
 })
